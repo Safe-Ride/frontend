ebdf555e86ee6f0132bd628f57e7af298a877bb2
daniloBritoo <danilo.brito@sptech.school>
2024-05-12 13:25:11 -0300

    feat: adicionado header como componente

--- safe-ride/src/pages/home/Home.jsx
+++ safe-ride/src/pages/home/Home.jsx
@@ -2,15 +2,17 @@ import React from "react";
 // import styles from "./Home.module.css";
 import NavBarTop from "../../components/NavBar/NavBarTop";
 import NavBarBot from "../../components/NavBar/NavBarBot";
+import Header from "../../components/Header/Header";
 
 const titulo = "clientes";
 
 const Home = () => {
   return (
     <>
-      <NavBarTop titulo={titulo} />
+    <Header />
+    
 
-      <NavBarBot />
+
     </>
   );
 };
