93db4b8af8f3411c4bcfb0146691aa519685ec0c
Bruno Henrique <bruno.lima@sptech.school>
2024-06-05 09:37:50 -0300

    [SF-92] FIX: Dashboard de boletos por situação para motoristas

--- src/api.js
+++ src/api.js
@@ -1,13 +1,8 @@
 import axios from "axios";
 
-// const api = axios.create({
-//   // baseURL: process.env.REACT_APP_API_URL,
-//   baseURL: "https://661ef2e016358961cd932dc5.mockapi.io/usuarios",
-// });
-
-const apiDonut = axios.create({
+const api = axios.create({
   // baseURL: process.env.REACT_APP_API_URL,
-  baseURL: "https://661ef2e016358961cd932dc5.mockapi.io/musicas",
+  baseURL: "https://661ef2e016358961cd932dc5.mockapi.io/usuarios",
 });
 
-export default apiDonut;
+export default api;
