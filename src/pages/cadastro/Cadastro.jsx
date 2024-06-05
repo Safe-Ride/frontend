ff65926baeeae68c78c65c6e7685beb954c10d4d
felipeguerrino-sptech <felipe.guerrino@sptech.school>
2024-06-05 00:42:32 +0000

    feat: tela inicial de cadastro

--- src/pages/cadastro/Cadastro.jsx
+++ src/pages/cadastro/Cadastro.jsx
@@ -1,12 +1,21 @@
 import Logo from "../../components/Logo/Logo";
+import NavBarTop from "../../components/NavBar/NavBarTop";
 import styles from "./Cadastro.module.css";
 
 function Cadastro() {
-    return ( <>
-        <div className={styles["bg"]}>
-            <Logo />
+  return (
+    <>
+      <NavBarTop titulo={"CADASTRO"} />
+      <div className={styles["grid-container"]}>
+        <div className={styles["form"]}>
+          <Logo tamanho={"logo-grande"} />
+          <p>Conte-nos um pouco mais sobre você</p>
+          <button className={styles["btn-light"]}>Sou Responsável</button>
+          <button className={styles["btn-dark"]}>Sou Motorista</button>
         </div>
-    </> );
+      </div>
+    </>
+  );
 }
 
 export default Cadastro;
