315858bed56b2e777473166ef29bed91a16bdec6
Bruno Henrique <bruno.lima@sptech.school>
2024-06-05 09:32:19 -0300

    [SF-92] Dashboard de boletos por situação para motoristas

--- src/api.js
+++ src/api.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
-const api = axios.create({
+// const api = axios.create({
+//   // baseURL: process.env.REACT_APP_API_URL,
+//   baseURL: "https://661ef2e016358961cd932dc5.mockapi.io/usuarios",
+// });
+
+const apiDonut = axios.create({
   // baseURL: process.env.REACT_APP_API_URL,
-  baseURL: "https://661ef2e016358961cd932dc5.mockapi.io/usuarios",
+  baseURL: "https://661ef2e016358961cd932dc5.mockapi.io/musicas",
 });
 
-export default api;
+export default apiDonut;
--- src/components/Motorista/VisaoGeral/Graficos/DonutChart.jsx
+++ src/components/Motorista/VisaoGeral/Graficos/DonutChart.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+import apiDonut from "../../../../api";
+import React, { useState } from "react";
 import ReactEcharts from "echarts-for-react";
 
 const DonutChart = () => {
+  const [qtdPago, setQtdPago] = useState("");
+  const [qtdPendente, setQtdPendente] = useState("");
+  const [qtdAtrasado, setQtdAtrasado] = useState("");
+
+  function recuperarInformacoesCliente() {
+    apiDonut
+      .get()
+      .then((response) => {
+        const { data } = response;
+        console.log(data[0]);
+        const { pago, pendente, atrasado } = data[0];
+
+        setQtdPago(pago);
+        setQtdPendente(pendente);
+        setQtdAtrasado(atrasado);
+      })
+      .catch(() => {
+        console.log("Deu erro, tente novamente! ");
+      });
+  }
+
+  recuperarInformacoesCliente();
+  // useEffect(() => {
+  //   recuperarInformacoesCliente();
+  // }, []);
+
   const getOption = () => {
     return {
+      tooltip: {
+        trigger: "item",
+      },
+      // legend: {
+      //   orient: "vertical",
+      //   left: "left",
+      // },
       series: [
         {
           name: "Boletos",
@@ -11,23 +45,103 @@ const DonutChart = () => {
           radius: ["40%", "80%"],
           avoidLabelOverlap: false,
           label: {
-            show: false,
+            show: false, // Esconde rótulos padrão
             position: "center",
           },
           emphasis: {
             label: {
-              show: true,
+              show: false, // Mostra rótulos ao passar o mouse
               fontSize: "20",
               fontWeight: "bold",
             },
           },
           labelLine: {
-            show: false,
+            show: false, // Esconde linhas de rótulo
           },
           data: [
-            { value: 4, name: "Pendente", itemStyle: { color: "#ff944d" } },
-            { value: 2, name: "Atrasado", itemStyle: { color: "#ff6363" } },
-            { value: 6, name: "Pago", itemStyle: { color: "#64adad" } },
+            {
+              value: qtdPago,
+              name: "Pago",
+              itemStyle: {
+                color: {
+                  type: "linear",
+                  x: 0,
+                  y: 0,
+                  x2: 0,
+                  y2: 1,
+                  colorStops: [
+                    { offset: 0, color: "#64adad" },
+                    { offset: 0.1, color: "#64adad" },
+                    { offset: 0.1, color: "#018789" },
+                    { offset: 0.2, color: "#018789" },
+                    { offset: 0.2, color: "#64adad" },
+                    { offset: 0.3, color: "#64adad" },
+                    { offset: 0.3, color: "#018789" },
+                    { offset: 0.4, color: "#018789" },
+                    { offset: 0.4, color: "#64adad" },
+                    { offset: 0.5, color: "#64adad" },
+                    { offset: 0.5, color: "#018789" },
+                    { offset: 0.6, color: "#018789" },
+                    { offset: 0.6, color: "#64adad" },
+                    { offset: 0.7, color: "#64adad" },
+                    { offset: 0.7, color: "#018789" },
+                    { offset: 0.8, color: "#018789" },
+                    { offset: 0.8, color: "#64adad" },
+                    { offset: 0.9, color: "#64adad" },
+                    { offset: 0.9, color: "#018789" },
+                    { offset: 1, color: "#018789" },
+                  ],
+                },
+              },
+            },
+            {
+              value: qtdPendente,
+              name: "Pendente",
+              itemStyle: {
+                color: {
+                  type: "linear",
+                  x: 0,
+                  y: 0,
+                  x2: 1,
+                  y2: 1,
+                  colorStops: [
+                    { offset: 0, color: "#ff944d" },
+                    { offset: 0.2, color: "#ff944d" },
+                    { offset: 0.2, color: "#f60" },
+                    { offset: 0.4, color: "#f60" },
+                    { offset: 0.4, color: "#ff944d" },
+                    { offset: 0.6, color: "#ff944d" },
+                    { offset: 0.6, color: "#f60" },
+                    { offset: 0.8, color: "#f60" },
+                    { offset: 0.8, color: "#ff944d" },
+                    { offset: 1, color: "#ff944d" },
+                  ],
+                },
+              },
+            },
+            {
+              value: qtdAtrasado,
+              name: "Atrasado",
+              itemStyle: {
+                color: {
+                  type: "linear",
+                  x: 0,
+                  y: 0,
+                  x2: 1,
+                  y2: 0,
+                  colorStops: [
+                    { offset: 0, color: "#ff6363" },
+                    { offset: 0.25, color: "#ff6363" },
+                    { offset: 0.25, color: "#e50101" },
+                    { offset: 0.5, color: "#e50101" },
+                    { offset: 0.5, color: "#ff6363" },
+                    { offset: 0.75, color: "#ff6363" },
+                    { offset: 0.75, color: "#e50101" },
+                    { offset: 1, color: "#e50101" },
+                  ],
+                },
+              },
+            },
           ],
         },
       ],
@@ -37,7 +151,7 @@ const DonutChart = () => {
   return (
     <ReactEcharts
       option={getOption()}
-      style={{ height: "auto", width: "100%" }}
+      style={{ height: "25vh", width: "100%" }}
     />
   );
 };
