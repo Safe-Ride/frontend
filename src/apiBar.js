671ae465506fa87cca0f4fea09e87e028bb74028
Bruno Henrique <bruno.lima@sptech.school>
2024-06-10 16:56:33 -0300

    FIX: excluindo arquivos

--- src/apiBar.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const apiBar = axios.create({
-  // baseURL: process.env.REACT_APP_API_URL,
-  baseURL: "http://localhost:8080/pagamentos/pagamentos-total-efetuados",
-});
-
-export default apiBar;
--- src/pages/cadastro/Cadastro.jsx
+++
@@ -1,7 +0,0 @@
-function Cadastro() {
-    return ( <>
-    
-    </> );
-}
-
-export default Cadastro;
