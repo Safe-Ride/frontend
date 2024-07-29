3fffd269fbb374728723dc2bf116f222bcc6680d
lucasrocha2704 <lucas.rocha@sptech.school>
2024-07-29 20:50:57 -0300

    feat: criação de elementos para cards na tela de trajeto do motorista

---
+++ src/components/Motorista/CardTrajeto/CardTrajeto.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./CardTrajeto.module.css";
+import OnOff from "../CheckBoxONOFF/OnOff"
+
+const CARD = () => {
+    return (
+        <div className={styles['container']}>
+            <h3 className={styles['text']}>Etec Getúlio Vargas</h3>
+            <h3 className={styles['text']}>IDA</h3>
+            <h3 className={styles['turno']}>M</h3>
+            {/* <OnOff/> */}
+        </div>
+    );
+};
+
+export default CARD;
--- src/components/Motorista/TrajetosGerais/TrajetosGerais.jsx
+++ src/components/Motorista/TrajetosGerais/TrajetosGerais.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./TrajetosGerais.module.css";
+import Card from '../CardTrajeto/CardTrajeto'
 
 const TRJG = () => {
     return (
@@ -7,6 +8,10 @@ const TRJG = () => {
             <div className={styles["header"]}>
                 <div className={styles["title"]}>TRAJETOS</div>
                 <input className={styles["search"]} type="text" placeholder="Pesquisar" />
+            </div>
+            <Card />
+            <div className={styles['container']}>
+                <h3 className={styles['text']}>+ Adicionar novo trajeto</h3>
             </div>
         </div>
     );
