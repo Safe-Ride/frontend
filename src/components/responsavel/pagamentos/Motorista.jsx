2af60fa6e884ea1c84d24d336a0f75f03778ed79
Bruno Henrique <bruno.lima@sptech.school>
2024-08-28 18:24:26 -0300

    [SR-43] Filtro de contrato por ano

--- src/components/responsavel/pagamentos/Motorista.jsx
+++ src/components/responsavel/pagamentos/Motorista.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import styles from "./Motorista.module.css";
 import FormatarData from "../../../utils/functions/FormatarData";
+import FotoPerfil from "../../../utils/functions/FormatarData";
 
 const Motorista = ({ motorista, pagamento }) => {
   return (
     <div className={styles["motorista"]}>
-      <img src={motorista.foto} alt="" className={styles["foto"]} />
+      <img src={FotoPerfil(motorista.foto)} alt="" className={styles["foto"]} />
       <div className={styles["campos"]}>
         <h2 className={styles["nome"]}>{motorista.nome}</h2>
         <p className={styles["ultimo-pagamento"]}>
