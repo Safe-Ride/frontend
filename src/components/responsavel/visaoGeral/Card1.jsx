6e819f4deac9825d14f066a42fc9576ce340ae15
Lucas1Ferreira <lufebetico09@gmail.com>
2024-09-09 23:16:03 -0300

    feat: css and images aded

--- src/components/responsavel/visaoGeral/Card1.jsx
+++ src/components/responsavel/visaoGeral/Card1.jsx
@@ -1,67 +1,48 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./Card1.module.css";
-import icoEditar from "../../../utils/assets/dependentes/lapis.png";
-import icoConfirmar from "../../../utils/assets/perfil/done.png";
+import icoCasa from "../../../utils/assets/dependentes/casa.png";
+import icoEscola from "../../../utils/assets/dependentes/escola.png";
 
-const CardVisao = ({ icone }) => {
-    const [iconeEditar, setIconeEditar] = useState(icoEditar);
-    const [isEditable, setIsEditable] = useState(false);
-    const [inputValue, setInputValue] = useState("rua haddock lobo,595 Cerqueira César, São Paulo - SP, 01414-001");
+const CardVisao = () => {
+  return (
+    <div className={styles["Card"]}>
+      <div className={styles["Information"]}>
+        <p className={styles["nameDependent"]}>Bruno Henrique</p>
+        <p className={styles["Status"]}>Voltando para casa</p>
+      </div>
 
-    const habilitarModoEditar = () => {
-        setIsEditable(!isEditable); // Alterna o estado de edição
-        setIconeEditar(isEditable ? icoEditar : icoConfirmar); // Alterna o ícone entre lápis e confirmar
-    };
-
-    const handleInputChange = (event) => {
-        setInputValue(event.target.value); // Atualiza o valor do input conforme o usuário digita
-    };
-
-    return (
-        <div className={styles["Card"]}>
-            <div className={styles["Information"]}>
-                <p className={styles["nameDependent"]}>Bruno Henrique</p>
-                <p className={styles["Status"]}>Voltando p casa</p>
-            </div>
-            <div className={styles["exitBox"]}>
-                <div className={styles['icone']}>
-                    <img className={styles['icone-img']} src={icone} alt="ico" />
-                </div>
-                <div className={styles["div-endereco-exit"]}>
-                    {isEditable ? (
-                        <input 
-                            className={styles["input-endereco-exit"]}
-                            value={inputValue}
-                            onChange={handleInputChange}
-                            autoFocus
-                        />
-                    ) : (
-                        <p className={styles["p-endereco-exit"]}>{inputValue}</p>
-                    )}
-                </div>
-                <div className={styles["div-time-exit"]}>
-                    <p className={styles["time-exit"]}>12:00</p>
-                </div>
-            </div>
-            <div className={styles["BoxArrow"]}>
-                <img className={styles["Arrow"]} src="" alt="" />
-            </div>
-            <div className={styles["returnBOX"]}>
-                <div className={styles["img-status-return"]}>
-                    <img src="" alt="" />
-                </div>
-            </div>
-            <div className={styles["footer"]}>
-                <img 
-                    className={styles["img-editar"]}
-                    src={iconeEditar}
-                    onClick={habilitarModoEditar}
-                    alt="Editar"
-                />
-                <p>Ver Dependente</p>
-            </div>
+      <div className={styles["exitBox"]}>
+        <div className={styles["icone"]}>
+          {/* Exibe o ícone da casa */}
+          <img className={styles["icone-img"]} src={icoCasa} alt="Ícone da casa" />
         </div>
-    );
+        <div className={styles["div-endereco-exit"]}>
+          <p className={styles["p-endereco-exit"]}>
+            Rua Haddock Lobo, 595, Cerqueira César, São Paulo - SP, 01414-001
+          </p>
+        </div>
+        <div className={styles["div-time-exit"]}>
+          <p className={styles["time-exit"]}>12:00</p>
+        </div>
+      </div>
+
+      <div className={styles["BoxArrow"]}>
+        <img className={styles["Arrow"]} src="" alt="" />
+      </div>
+
+      <div className={styles["returnBOX"]}>
+        <div className={styles["img-status-return"]}>
+        <div className={styles["icone"]}>
+          <img src={icoEscola} alt="Ícone da escola" />
+          </div>
+        </div>
+      </div>
+
+      <div className={styles["footer"]}>
+        <p>Ver Dependente</p>
+      </div>
+    </div>
+  );
 };
 
 export default CardVisao;
--- src/pages/responsavel/visaoGeralResponsavel/visaoGeralResponsavel.jsx
+++ src/pages/responsavel/visaoGeralResponsavel/visaoGeralResponsavel.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-// import styles from "./TempoReal.module.css";
 import NavBarTop from "../../../components/NavBar/NavBarTop";
 import NavBarBot from "../../../components/NavBar/NavBarBot";
 import CardVisao from "../../../components/responsavel/visaoGeral/Card1";
@@ -14,17 +13,19 @@ function returnIconeStatus(status) {
       return icoEscola;
     case "casa":
       return icoCasa;
+    default:
+      return icoCasa; // Adicionando um valor padrão
   }
 }
 
 const VisaoGeralResponsavel = () => {
+  // Simular status para este exemplo
+  const status = "escola"; // Aqui você pode passar dinamicamente o status desejado
+
   return (
     <>
       <NavBarTop titulo={titulo} />
-        
-        <CardVisao>
-          
-        </CardVisao>
+      <CardVisao icone={returnIconeStatus(status)} />
       <NavBarBot />
     </>
   );
