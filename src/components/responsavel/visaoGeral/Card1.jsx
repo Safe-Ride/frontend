68df1b45e5e74eb380cdda70798bc26f1bc1b378
Lucas1Ferreira <lufebetico09@gmail.com>
2024-09-19 09:31:49 -0300

    feat: add

--- src/components/responsavel/visaoGeral/Card1.jsx
+++ src/components/responsavel/visaoGeral/Card1.jsx
@@ -1,54 +1,83 @@
 import React from "react";
 import styles from "./Card1.module.css";
-import icoCasa from "../../../utils/assets/dependentes/casa.png"; // ícone de casa
-import icoEscola from "../../../utils/assets/dependentes/escola.png"; // ícone de escola
-import setaAmarela from "../../../utils/assets/dependentes/Arrow 2.png"; //
+import icoCasa from "../../../utils/assets/dependentes/casa.png";
+import icoEscola from "../../../utils/assets/dependentes/escola.png";
+import setaAmarela from "../../../utils/assets/dependentes/Arrow 2.png";
+import { useNavigate } from "react-router-dom";
 
-const CardVisao = () => {
+const CardVisao = ({ id,nomeDependente, status, enderecoSaida, horarioSaida, enderecoRetorno, horarioRetorno }) => {
+  const navigate = useNavigate();
+
+  // Definindo a ordem dos ícones e endereços com base no status
+  const statusConfig = {
+    "Voltando para Casa": {
+      firstIcon: icoEscola,
+      firstAddress: enderecoSaida,
+      firstTime: horarioSaida,
+      secondIcon: icoCasa,
+      secondAddress: enderecoRetorno,
+      secondTime: horarioRetorno,
+    },
+    "Indo para Escola": {
+      firstIcon: icoCasa,
+      firstAddress: enderecoSaida,
+      firstTime: horarioSaida,
+      secondIcon: icoEscola,
+      secondAddress: enderecoRetorno,
+      secondTime: horarioRetorno,
+    },
+    "Na Escola": {
+      firstIcon: icoCasa,
+      firstAddress: enderecoSaida,
+      firstTime: horarioSaida,
+      secondIcon: icoEscola,
+      secondAddress: enderecoRetorno,
+      secondTime: horarioRetorno,
+    },
+  };
+
+  const { firstIcon, firstAddress, firstTime, secondIcon, secondAddress, secondTime } = statusConfig[status];
+
   return (
     <div className={styles["Card"]}>
       <div className={styles["Information"]}>
-        <p className={styles["nameDependent"]}>BRUNO HENRIQUE</p>
-        <p className={styles["Status"]}>Voltando para Casa</p>
+        <p className={styles["nameDependent"]}>{nomeDependente}</p>
+        <p className={styles["Status"]}>{status}</p>
       </div>
 
+      {/* Primeira seção com o primeiro ícone e endereço */}
       <div className={styles["exitBox"]}>
         <div className={styles["icone"]}>
-         
-          <img className={styles["icone-img"]} src={icoEscola} alt="Ícone da escola" />
+          <img className={styles["icone-img"]} src={firstIcon} alt="Ícone" />
         </div>
         <div className={styles["div-endereco-exit"]}>
-          <p className={styles["p-endereco-exit"]}>
-            Rua Haddock Lobo, 595 - Cerqueira César, São Paulo - SP, 01414-001
-          </p>
+          <p className={styles["p-endereco-exit"]}>{firstAddress}</p>
         </div>
         <div className={styles["div-time-exit"]}>
-          <p className={styles["time-exit"]}>12:00</p>
+          <p className={styles["time-exit"]}>{firstTime}</p>
         </div>
       </div>
 
+      {/* Seta entre os dois ícones */}
       <div className={styles["BoxArrow"]}>
-        
         <img className={styles["Arrow"]} src={setaAmarela} alt="Seta" />
       </div>
 
+      {/* Segunda seção com o segundo ícone e endereço */}
       <div className={styles["returnBox"]}>
         <div className={styles["icone"]}>
-          {/* Ícone da casa */}
-          <img className={styles["icone-img"]} src={icoCasa} alt="Ícone da casa" />
+          <img className={styles["icone-img"]} src={secondIcon} alt="Ícone" />
         </div>
         <div className={styles["div-endereco-return"]}>
-          <p className={styles["p-endereco-return"]}>
-            Rua Haddock Lobo, 595 - Cerqueira César, São Paulo - SP
-          </p>
+          <p className={styles["p-endereco-return"]}>{secondAddress}</p>
         </div>
         <div className={styles["div-time-return"]}>
-          <p className={styles["time-return"]}>12:00</p>
+          <p className={styles["time-return"]}>{secondTime}</p>
         </div>
       </div>
 
       <div className={styles["footer"]}>
-        <p>Ver Dependente</p>
+        <p onClick={() => navigate(`/responsavel/dependentes/${id}`)}>Ver Dependente</p>
       </div>
     </div>
   );
