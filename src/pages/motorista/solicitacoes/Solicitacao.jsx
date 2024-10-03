64bc7646a2cf2fe84b28ec0956c3d1b1d98f3a05
Hideki19 <vitor.nakamura@sptech.school>
2024-10-03 20:04:30 -0300

    refactor: Página Solicitação Motorista

--- src/pages/motorista/solicitacoes/Solicitacao.jsx
+++ src/pages/motorista/solicitacoes/Solicitacao.jsx
@@ -3,9 +3,14 @@ import NavBarBot from "../../../components/NavBar/NavBarBot"
 import NavBarTop from "../../../components/NavBar/NavBarTop"
 import Box from "../../../components/responsavel/dependentes/Box/Box"
 import CardInfo from "../../../components/responsavel/dependentes/perfilDependente/CardInfo/CardInfo"
+import FotoPerfil from "../../../utils/functions/FotoPerfil"
 import icoCasa from "../../../utils/assets/dependentes/casa.png"
 import icoContrato from "../../../utils/assets/dependentes/contrato.png"
+import icoRua from "../../../utils/assets/dependentes/road.png"
+import icoRelogio from "../../../utils/assets/dependentes/relogio.png"
+import icoCalendario from "../../../utils/assets/dependentes/calendario.png"
 import { useState } from "react"
+
 
 const Solicitacao = () => {
     const titulo = "Solicitações"
@@ -30,16 +35,64 @@ const Solicitacao = () => {
                             gap: 16 + "px",
                         }}
                     >
-                        <img className={styles["foto-perfil"]} src="" alt="" />
-                        <h2 className={styles["nome"]}>Bruno Henrique</h2>
-                        <p className={styles["dependente"]}>
-                            Dependente: Lucas Neves
-                        </p>
+                        <img className={styles["foto-perfil"]} src={FotoPerfil()} alt="" />
+                        <div>
+                            <h2 className={styles["nome"]}>Bruno Henrique</h2>
+                            <p className={styles["dependente"]}>
+                                Dependente: Lucas Neves
+                            </p>
+                        </div>
+
                         <CardInfo
                             icone={icoCasa}
                             categoria={"Endereço:"}
                             info={"Rua Haddock Lobo, 595 São Paulo - SP"}
                         />
+                        <CardInfo
+                            icone={icoRua}
+                            categoria={"Ida e/ou Volta:"}
+                            info={"Ida"}
+                        />
+                        <CardInfo
+                            icone={icoCalendario}
+                            categoria={"Dias da Semana:"}
+                            info={"Segunda, Terça, Quarta, Quinta, Sexta"}
+                        />
+                        <CardInfo
+                            icone={icoRelogio}
+                            categoria={"Período:"}
+                            info={"Manhã"}
+                        />
+                        <div className={styles['card']}>
+                            <div className={styles['icone']}>
+                                <img className={styles['icone-img']} src={icoRelogio} alt="ico" />
+                            </div>
+                            <div className={styles['content']}>
+                                <span className={styles['categoria']}>Horário: </span>
+                                <div className={styles['container-horario']}>
+
+                                    <div className={styles['ipt-horario']}>
+                                        <span>Ida:</span>
+                                        <input
+                                            placeholder="dd/MM/YYYY"
+                                            className={styles['ipt-contrato']}
+                                            onChange={handleInputChange}
+                                            type="time"
+                                        />
+                                    </div>
+                                    <div className={styles['ipt-horario']}>
+                                        <span>Volta:</span>
+                                        <input
+                                            placeholder="dd/MM/YYYY"
+                                            className={styles['ipt-contrato']}
+                                            onChange={handleInputChange}
+                                            type="time"
+                                        />
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
                         <div className={styles['card']}>
                             <div className={styles['icone']}>
                                 <img className={styles['icone-img']} src={icoContrato} alt="ico" />
@@ -75,7 +128,7 @@ const Solicitacao = () => {
                                 <img className={styles['icone-img']} src={icoContrato} alt="ico" />
                             </div>
                             <div className={styles['content']}>
-                                <span className={styles['categoria']}>Valor:</span>
+                                <span className={styles['categoria']} id={styles['categoria-valor']}>Valor:</span>
                                 <div style={{ width: 100 + "%" }}>
 
                                     <div className={styles['campo-editar']}>
