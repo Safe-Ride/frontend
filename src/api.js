9996c2ea44ec93607a8f1f96e99828895bbd7a4b
Bruno Henrique <bruno.lima@sptech.school>
2024-10-24 17:23:58 -0300

    fix: chamadas backend

--- src/api.js
+++ src/api.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-  headers: {
-    'Authorization': `Bearer ${sessionStorage.token}`,
-    'Content-Type': 'application/json'
-  }
+  // headers: {
+  //   'Authorization': `Bearer ${sessionStorage.token}`,
+  //   'Content-Type': 'application/json'
+  // }
   // baseURL: "https://661ef2e016358961cd932dc5.mockapi.io/usuarios",
 });
 
--- src/pages/motorista/solicitacoes/Solicitacoes.jsx
+++ src/pages/motorista/solicitacoes/Solicitacoes.jsx
@@ -1,43 +1,56 @@
-import { useNavigate } from "react-router-dom"
-import OpcaoCliente from "../../../components/motorista/clientes/OpcaoCliente"
-import Pesquisa from "../../../components/motorista/clientes/Pesquisa"
-import NavBarBot from "../../../components/NavBar/NavBarBot"
-import NavBarTop from "../../../components/NavBar/NavBarTop"
-import { useEffect, useState } from "react"
-import api from "../../../api"
-import FotoPerfil from "../../../utils/functions/FotoPerfil"
+import { useNavigate } from "react-router-dom";
+import OpcaoCliente from "../../../components/motorista/clientes/OpcaoCliente";
+import Pesquisa from "../../../components/motorista/clientes/Pesquisa";
+import NavBarBot from "../../../components/NavBar/NavBarBot";
+import NavBarTop from "../../../components/NavBar/NavBarTop";
+import { useEffect, useState } from "react";
+import api from "../../../api";
+import FotoPerfil from "../../../utils/functions/FotoPerfil";
 
 const Solicitacoes = () => {
-    const titulo = "Solicitações"
-    const navigate = useNavigate();
-    const [solicitacoes, setSolicitacoes] = useState([]);
+  const titulo = "Solicitações";
+  const navigate = useNavigate();
+  const [solicitacoes, setSolicitacoes] = useState([]);
 
-    useEffect(() => {
-        api.get(`/solicitacoes/motorista/${sessionStorage.ID_USUARIO}`)
-            .then((res) => {
-                const data = res.data;
-                console.log(data);
-                setSolicitacoes(data);
-            })
-            .catch((err) => console.error(err));
-    }, [])
+  useEffect(() => {
+    api
+      .get(`/solicitacoes/motorista/${sessionStorage.ID_USUARIO}`, {
+        headers: {
+          Authorization: `Bearer ${sessionStorage.token}`,
+        },
+      })
+      .then((res) => {
+        const data = res.data;
+        console.log(data);
+        setSolicitacoes(data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
 
-    return (
-        <>
-            <NavBarTop titulo={titulo} />
-            <Pesquisa />
-            {solicitacoes.map((solicitacao) => {
-                if (solicitacao.status == "PENDENTE_MOTORISTA") {
-                    return (
-                        <div onClick={() => navigate(`/motorista/solicitacoes/${solicitacao.id}`)}>
-                            <OpcaoCliente foto={FotoPerfil(solicitacao.responsavel.imagem.caminho)} nome={solicitacao.responsavel.nome} />
-                        </div>
-                    )
+  return (
+    <>
+      <NavBarTop titulo={titulo} />
+      <Pesquisa />
+      {solicitacoes &&
+        solicitacoes.map((solicitacao) => {
+          if (solicitacao.status == "PENDENTE_MOTORISTA") {
+            return (
+              <div
+                onClick={() =>
+                  navigate(`/motorista/solicitacoes/${solicitacao.id}`)
                 }
-            })}
-            <NavBarBot />
-        </>
-    )
-}
+              >
+                <OpcaoCliente
+                  foto={FotoPerfil(solicitacao.responsavel.imagem.caminho)}
+                  nome={solicitacao.responsavel.nome}
+                />
+              </div>
+            );
+          }
+        })}
+      <NavBarBot />
+    </>
+  );
+};
 
-export default Solicitacoes
+export default Solicitacoes;
